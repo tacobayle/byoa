8d56bd82f159212fd8afabaa0aee6166b6c00ee1
tacobayle <nicolas.bayle@gmail.com>
2021-11-23 08:50:35 +0000

    20211123_output

--- output.tf
+++ output.tf
@@ -25,31 +25,31 @@ output "Avi_password" {
 }
 
 output "Destroy_command_all" {
-  value = "\nssh -o StrictHostKeyChecking=no -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -t ubuntu@${vsphere_virtual_machine.destroy_env_vm.default_ip_address} 'cd aviAbsent ; ansible-playbook local.yml --extra-vars @${var.controller.aviCredsJsonFile}' ; sleep 5 ; terraform destroy -auto-approve -var-file=vcenter.json\n"
+  value = "ssh -o StrictHostKeyChecking=no -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -t ubuntu@${vsphere_virtual_machine.destroy_env_vm.default_ip_address} 'cd aviAbsent ; ansible-playbook local.yml --extra-vars @${var.controller.aviCredsJsonFile}' ; sleep 5 ; terraform destroy -auto-approve -var-file=vcenter.json"
   description = "command to destroy the infra"
 }
 
 output "Destroy_command_wo_tf" {
-  value = "\nssh -o StrictHostKeyChecking=no -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -t ubuntu@${vsphere_virtual_machine.destroy_env_vm.default_ip_address} './destroyAvi.sh'\n"
+  value = "ssh -o StrictHostKeyChecking=no -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -t ubuntu@${vsphere_virtual_machine.destroy_env_vm.default_ip_address} './destroyAvi.sh'"
   description = "command to destroy the avi config"
 }
 
 output "ako_install_command_to_apply_on_master_node" {
-  value = "\nhelm --debug install ako/ako --generate-name --version ${var.vmw.kubernetes.clusters[0].ako.version} -f values.yml --namespace=${var.vmw.kubernetes.clusters[0].ako.namespace} --set avicredentials.username=admin --set avicredentials.password=$avi_password\n"
+  value = "helm --debug install ako/ako --generate-name --version ${var.vmw.kubernetes.clusters[0].ako.version} -f values.yml --namespace=${var.vmw.kubernetes.clusters[0].ako.namespace} --set avicredentials.username=admin --set avicredentials.password=$avi_password"
 }
 
 output "ssh_connect_to_client_VM" {
-  value = "\nssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@${vsphere_virtual_machine.client.default_ip_address}\n"
+  value = "ssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@${vsphere_virtual_machine.client.default_ip_address}"
 }
 
 output "ssh_connect_to_client_K8s_cluster1_master_node" {
-  value = "\nssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@${vsphere_virtual_machine.master[0].default_ip_address}\n"
+  value = "ssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@${vsphere_virtual_machine.master[0].default_ip_address}"
 }
 
 output "ssh_connect_to_client_K8s_cluster2_master_node" {
-  value = "\nssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@${vsphere_virtual_machine.master[1].default_ip_address}\n"
+  value = "ssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@${vsphere_virtual_machine.master[1].default_ip_address}"
 }
 
 output "ssh_connect_to_any_vm" {
-  value = "\nssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@<VM_IP>\n"
+  value = "ssh -i ~/.ssh/${var.ssh_key.private_key_basename}-${var.vcenter.folder}.pem -o StrictHostKeyChecking=no ubuntu@<VM_IP>"
 }
